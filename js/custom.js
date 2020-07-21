9c7e7d07d9f7a6ba66a826de7cab9f89e7fa86cb
Romulo Furtado <romulo.furtado@hotmail.com>
2020-07-21 22:12:41 +1000

    updates

--- js/custom.js
+++ js/custom.js
@@ -10,11 +10,6 @@ $(function () {
     duration: 600,
     closeOnClick: true,
   });
-
-  // //Carousel Speed
-  // $("#welcome-slide-carousel").carousel({
-  //   interval: 3000,
-  // });
   /*--------------------------
     scrollUp
     ---------------------------- */
@@ -31,59 +26,6 @@ $(function () {
     var theYear = new Date().getFullYear();
     $("#year").html(theYear);
   });
-
-  // /*------------------------------------------------------------------
-  //     Coming Soon Coutdown
-  //   ------------------------------------------------------------------*/
-  // $("[data-countdown]").each(function () {
-  //   var $this = $(this),
-  //     finalDate = $(this).data("countdown");
-  //   $this.countdown(finalDate, function (event) {
-  //     $this.html(
-  //       event.strftime(
-  //         '<div class="cntdown"><span class="days"><strong>%-D</strong><p>Days</p></span></div><div class="cntdown"><span class="hour"><strong> %-H</strong><p>Hours</p></span></div> <div class="cntdown"><span class="minutes"><strong>%M</strong> <p>MINUTES</p></span></div><div class="cntdown"><span class="second"><strong> %S</strong><p>SECONDS</p></span></div>'
-  //       )
-  //     );
-  //   });
-  // });
-  /*---------------------------------------------------------------------
-    		Magnific Popup 
-    ------------------------------------------------------------------------*/
-  // if ($(".front-gallery, .gallery-section").length) {
-  //   $(".front-gallery, .gallery-section").magnificPopup({
-  //     delegate: "a",
-  //     type: "image",
-  //     tLoading: "Loading image #%curr%...",
-  //     mainClass: "mfp-img-mobile",
-  //     gallery: {
-  //       enabled: true,
-  //       navigateByImgClick: true,
-  //       preload: [0, 1],
-  //     },
-  //     image: {
-  //       tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-  //       titleSrc: function (item) {
-  //         return item.el.attr("title") + "<small>by sbtechnosoft</small>";
-  //       },
-  //     },
-  //   });
-  // }
-  // if ($(".image-popup-no-margins").length) {
-  //   $(".image-popup-no-margins").magnificPopup({
-  //     type: "image",
-  //     closeOnContentClick: true,
-  //     closeBtnInside: false,
-  //     fixedContentPos: true,
-  //     mainClass: "mfp-no-margins mfp-with-zoom",
-  //     image: {
-  //       verticalFit: true,
-  //     },
-  //     zoom: {
-  //       enabled: true,
-  //       duration: 300,
-  //     },
-  //   });
-  // }
 
   /*---------------------------------------------------------------------
     Galeria Post Hove Effect for Caption Script
@@ -138,44 +80,6 @@ $(function () {
     },
   });
   /*------------------------------------------------------------------
-    Owl Carousel for Testemunhos
-	------------------------------------------------------------------*/
-  // var owl = $("#testimonials");
-  // owl.owlCarousel({
-  //   nav: true,
-  //   margin: 10,
-  //   loop: true,
-  //   responsive: {
-  //     0: {
-  //       items: 1,
-  //     },
-  //     450: {
-  //       items: 1,
-  //     },
-  //     600: {
-  //       items: 1,
-  //     },
-  //     700: {
-  //       items: 1,
-  //     },
-  //     1000: {
-  //       items: 1,
-  //     },
-  //     1200: {
-  //       items: 1,
-  //     },
-  //     1400: {
-  //       items: 1,
-  //     },
-  //     1600: {
-  //       items: 1,
-  //     },
-  //   },
-  // });
-  // $(".owl-prev").html('<i class="fa fa-angle-left"></i>');
-  // $(".owl-next").html('<i class="fa fa-angle-right"></i>');
-
-  /*------------------------------------------------------------------
     Header Navigation
     ------------------------------------------------------------------*/
   if ($(window).width() > 767) {
@@ -191,14 +95,6 @@ $(function () {
   }
 });
 
-/*------------------------------------------------------------------
-WOW
-------------------------------------------------------------------*/
-// wow = new WOW({
-//   animateClass: "animated",
-//   offset: 100,
-// });
-// wow.init();
 /*------------------------------------------------------------------
  Loader 
 ------------------------------------------------------------------*/
@@ -228,19 +124,7 @@ jQuery(window).on("load scroll", function () {
 /*------------------------------------------------------------------
     Perguntas Frequentes
     ------------------------------------------------------------------*/
-$(".panel-heading a").on("click", function () {
-  $(".panel-heading").removeClass("active");
-  $(this).parents(".panel-heading").addClass("active");
-});
-/*------------------------------------------------------------------
-Count Down
-------------------------------------------------------------------*/
-// if ($(".count-down").length) {
-//   var year = parseInt($(".count-down").attr("data-countdown-year"), 10);
-//   var month = parseInt($(".count-down").attr("data-countdown-month"), 3) - 1;
-//   var day = parseInt($(".count-down").attr("data-countdown-day"), 10);
-//   $(".count-down").countdown({
-//     until: new Date(year, month, day),
-//     padZeroes: true,
-//   });
-// }
+// $(".panel-heading a").on("click", function () {
+//   $(".panel-heading").removeClass("active");
+//   $(this).parents(".panel-heading").addClass("active");
+// });
